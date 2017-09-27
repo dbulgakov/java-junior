3bba75caf058be48d2654088f95a2213e8bd5ddb
GuntikAS <losyonochek@yandex.ru>
2017-09-27 17:41:53 +0300

    add method print

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -10,27 +10,30 @@ public class Logger {
     private static final String STRING_PREFIX = "string: ";
 
 
-
     public static void log(int message) {
-        System.out.println(PRIMITIVE_PREFIX + message);
+        print(PRIMITIVE_PREFIX + message);
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 
 
     public static void log(byte message) {
-        System.out.println(PRIMITIVE_PREFIX + message);
+        print(PRIMITIVE_PREFIX + message);
     }
     public static void log(Object message) {
-        System.out.println(REFERENCE_PREFIX + message);
+        print(REFERENCE_PREFIX + message);
     }
     public static void log(char message) {
-        System.out.println(CHAR_PREFIX + message);
+        print(CHAR_PREFIX + message);
     }
     public static void log(String message) {
-        System.out.println(STRING_PREFIX + message);
+        print(STRING_PREFIX + message);
     }
 
     public static void log(boolean message) {
-        System.out.println(PRIMITIVE_PREFIX + message);
+       print(PRIMITIVE_PREFIX + message);
     }
 
 }
