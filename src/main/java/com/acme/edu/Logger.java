ee67df673c1234fe400715247afa175493aac55b
GuntikAS <losyonochek@yandex.ru>
2017-09-27 16:59:16 +0300

    add type's prefixes

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,22 +1,36 @@
 package com.acme.edu;
-
+/**
+ * JavaDoc
+ */
 public class Logger {
-    public static void log(int message) {
-        System.out.println("imits " + message);
-        System.out.println("prits " + message);
-        System.out.println("prits " + message);
-        System.out.println("primits " + message);
-        System.out.println("prits " + message);
-        System.out.println("pmits " + message);
-        System.out.println("primits " + message);
 
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+    private static final String REFERENCE_PREFIX = "reference: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String STRING_PREFIX = "string: ";
+
+
+
+    public static void log(int message) {
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
+
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
+    }
+    public static void log(Object message) {
+        System.out.println(REFERENCE_PREFIX + message);
+    }
+    public static void log(char message) {
+        System.out.println(CHAR_PREFIX + message);
+    }
+    public static void log(String message) {
+        System.out.println(STRING_PREFIX + message);
     }
 
-    public static void main(String[] args) {
-        System.out.println("HQQQQQQ!!!");
+    public static void log(boolean message) {
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
+
 }
